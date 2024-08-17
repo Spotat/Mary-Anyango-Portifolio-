2089249842a91382965cfe197049ae1e92ad24fe
Ngash Garang <ngashj88@gmail.com>
2024-08-17 22:39:24 +0300

    changes

--- static/files/script/script.js
+++ static/files/script/script.js
@@ -1,8 +1,12 @@
-let currentIndex = 0;
-
+// const navToggle = document.getElementsByClassName('.nav-toggle');
+// const navLinks = document.getElementsByClassName('.nav-links');
+// navToggle.addEventListener('click', ()=> {
+//     navLinks.classList.toggle('active')
+// })
 function showSlide(direction) {
-    const slides = document.querySelector(".testimonial-slider");
-    const totalSlides = document.querySelectorAll(".testimonial");
+    let currentIndex = 0;
+    const slide = document.querySelector(".testimonial-slider");
+    const totalSlides = document.querySelectorAll(".testimonial").length;
     currentIndex += direction;
 
     if(currentIndex >= totalSlides.length) {
@@ -10,41 +14,42 @@ function showSlide(direction) {
     } else if(currentIndex < totalSlides.length) {
         currentIndex = totalSlides - 1 
     } 
-    slides.style.transform = `translateX(-${currentIndex * .5}%)`;
-}
+    slide.style.transform = `translateX(-${currentIndex * 20}%)`;
+    }
 setInterval(() => {
     showSlide(1)
-}, 50);
+}, 150);
 
 // reveal funs
-/*function reveal(){
-    const reveals = document.querySelectorAll(".reveal")
+
+function reveal() {
+    const reveals = document.querySelectorAll(".reveal");
 
     for (let i = 0; i < reveals.length; i++) {
         const windowHeight = window.innerHeight;
         const elementTop = reveals[i].getBoundingClientRect().top
-        const elementVisible = 150;
+        const elementVisibe = 150;
 
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add(".active")
+        if (elementTop < windowHeight - elementVisibe) {
+            reveals[i].classList.add("active")
         } else {
-            reveals[i].classList.remove(".active")
+            reveals[i].classList.remove("active")
         }
     }
 }
-window.addEventListener("scroll", reveal)
-*/
+window.addEventListener('scroll', reveal)
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('.show')
-        } else {
-            entry.target.classList.remove('.show')
-        };
-    });
-});
 
-const hiddenElements = document.querySelectorAll('.reveal');
-hiddenElements.forEach((el) => observer.observe(el))
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('.show')
+//         } else {
+//             entry.target.classList.remove('.show')
+//         };
+//     });
+// });
+
+// const hiddenElements = document.querySelectorAll('.reveal');
+// hiddenElements.forEach((el) => observer.observe(el))

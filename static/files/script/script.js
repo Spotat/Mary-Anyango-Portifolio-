0c88f22b0d5f920d47f0678056dcfc3f00c58baa
Spotat <ngashj88@gmail.com>
2024-10-23 21:04:25 -0700

    commit

--- static/files/script/script.js
+++ static/files/script/script.js
@@ -7,10 +7,14 @@
 function openNav() {
     document.getElementById("sideNav").style.width = "90%"
     document.getElementById("sideNav").style.opacity = "1"
+    document.getElementById("sideNav").style.zIndex = "99"
+    document.getElementById("sideNav").style.visibility = "visible"
 }
 function closeNav() {
     document.getElementById("sideNav").style.width = "0%"
     document.getElementById("sideNav").style.opacity = "0"
+    document.getElementById("sideNav").style.zIndex = "-99"
+    document.getElementById("sideNav").style.visibility = "hidden"
 }
 let currentIndex = 0;
 const slide = document.querySelector(".testimonial-slider");
